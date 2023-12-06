7ed7de5ee31cc2142e13de1ff29f758169fc903d
marcoding-78 <marcoding78@gmail.com>
2023-12-06 22:06:43 +0330

    add: change mod file and update main.go in example diretory

--- example/main.go
+++ example/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+	"github.com/mar-coding/swaggerui"
 	"log"
 	"net/http"
-
-	"github.com/flowchartsman/swaggerui"
 )
 
 //go:embed spec/petstore.yml
 var spec []byte
+var port = 3000
 
 func main() {
 	log.SetFlags(0)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/pet/", petHandler)
 	mux.Handle("/swagger/", http.StripPrefix("/swagger", swaggerui.Handler(spec)))
-	log.Println("serving on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Printf("Listening on port %d...", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/flowchartsman/swaggerui
+module github.com/mar-coding/swaggerui
 
-go 1.16
+go 1.21.3
+
+require github.com/flowchartsman/swaggerui v0.0.0-20221017034628-909ed4f3701b
